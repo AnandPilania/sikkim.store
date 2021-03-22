d55d113f4033c13d59ada90cff9e97efaecfca8f
Ray Nirola <hi@raynirola.com>
2021-03-23 01:17:09 +0530

    Fix: sign out link not secure warning in verify email view.

--- resources/views/livewire/user/auth/verify.blade.php
+++ resources/views/livewire/user/auth/verify.blade.php
@@ -8,11 +8,10 @@
                 </h2>
                 <div class="mt-2 text-sm text-center text-gray-600 leading-5 max-w">
                     Or
-                    <a href="{{ route('logout') }}"
-                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                    <button type="button" onclick="document.getElementById('logout-form').submit();"
                        class="font-medium text-green-600 hover:text-green-500 focus:outline-none focus:underline transition ease-in-out duration-150">
                         sign out
-                    </a>
+                    </button>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
